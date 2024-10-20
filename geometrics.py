d3c2671501195bb419c7cd03726209c6e6a31fc1
unknown <dolphin2410@outlook.kr>
2024-10-20 17:57:32 +0900

    check if parameters are valid
    check whether the two given points are identical in LinearGraph

--- geometrics.py
+++ geometrics.py
@@ -3,6 +3,9 @@ from settings import X_SIZE, Y_SIZE
 
 class LinearGraph:
     def __init__(self, p1, p2):
+        if p1[0] == p2[0] and p1[1] == p2[1]:
+            raise ValueError("The given two points are identical")
+        
         self.p1 = p1
         self.p2 = p2
 
